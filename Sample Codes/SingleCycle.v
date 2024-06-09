9bcddcd7616a4dc91404d2b6c27ccdedd3efcc65
Vaishnavi-Pai19 <133690178+Vaishnavi-Pai19@users.noreply.github.com>
2024-06-10 00:49:09 +0300

    Update and rename SingleCycleV1.v to SingleCycle.v

--- Sample Codes/SingleCycle.v
+++ Sample Codes/SingleCycle.v
@@ -1,7 +1,7 @@
 `define ALULEN  31
 //`include "memfile.dat"
 
-module SingleCycleV1 (input clk, reset,output [31:0] writedata, dataadr,output memwrite);
+module SingleCycle (input clk, reset,output [31:0] writedata, dataadr,output memwrite);
 	wire [31:0] pc, instr, readdata;
 	// instantiate processor and memories
 	mips mips (clk, reset, pc, instr, memwrite, dataadr,writedata, readdata);
@@ -19,7 +19,6 @@ module dmem (input clk, we,input [31:0] a, wd,output [31:0] rd);
 endmodule
 
 
-
 module imem (input [5:0] a,output [31:0] rd);
 	reg [31:0] RAM[63:0];
 	integer i;
@@ -31,7 +30,7 @@ module imem (input [5:0] a,output [31:0] rd);
 endmodule
 
 
-module  mips(input clk, reset,
+module mips (input clk, reset,
 				output [31:0] pc,
 				input [31:0] instr,	
 				output memwrite,
@@ -41,29 +40,29 @@ module  mips(input clk, reset,
 	wire memtoreg, branch,
 	alusrc, regdst, regwrite, jump;
 	wire [2:0] alucontrol;
-	controller c(instr[31:26], instr[5:0], zero,memtoreg, memwrite, pcsrc,alusrc, regdst, regwrite, jump,alucontrol);
-	datapath dp(clk, reset, memtoreg, pcsrc,alusrc, regdst, regwrite, jump,alucontrol,zero, pc, instr,aluout, writedata, readdata);
+	controller c(instr[31:26], instr[5:0], zero, memtoreg, memwrite, pcsrc, alusrc, regdst, regwrite, jump, alucontrol);
+	datapath dp(clk, reset, memtoreg, pcsrc, alusrc, regdst, regwrite, jump, alucontrol, zero, pc, instr, aluout, writedata, readdata);
 
 endmodule
 
 
 module controller (input [5:0] op, funct,
-						input zero,
-						output memtoreg, memwrite,
-						output pcsrc, alusrc,
-						output regdst, regwrite,
-						output jump,
-						output [2:0] alucontrol);
+					input zero,
+					output memtoreg, memwrite,
+					output pcsrc, alusrc,
+					output regdst, regwrite,
+					output jump,
+					output [2:0] alucontrol);
 						
 	wire [1:0] aluop;
 	wire branch;
 	maindec md (op, memtoreg, memwrite, branch,alusrc, regdst, regwrite, jump,aluop);
-	aludec ad (op,funct, aluop, alucontrol);
+	aludec ad (funct, aluop, alucontrol);
 	assign pcsrc = branch & zero;
 endmodule
 
 
-module maindec(input [5:0] op,
+module maindec (input [5:0] op,
 				output memtoreg, memwrite,
 				output branch, alusrc,
 				output regdst, regwrite,
@@ -71,34 +70,26 @@ module maindec(input [5:0] op,
 				output [1:0] aluop);
 				
 	reg [8:0] controls;
-	assign {regwrite, regdst, alusrc,branch, memwrite,memtoreg, jump, aluop} = controls;
+	assign {regwrite, regdst, alusrc, branch, memwrite, memtoreg, jump, aluop} = controls;
 	always @ (*)
 		case(op)
-			6'b000000: controls <= 9'b110000010; //Rtyp
+			6'b000000: controls < 9'b110000010; //Rtype
 			6'b100011: controls <=9'b101001000; //LW
 			6'b101011: controls <=9'b001010000; //SW
 			6'b000100: controls <=9'b000100001; //BEQ
 			6'b001000: controls <=9'b101000000; //ADDI
-			6'b001100: controls <=9'b101000000; //ANDI
-			6'b001101: controls <=9'b101000000; //ORI
-			6'b001110: controls <=9'b101000000; //XORI
 			6'b000010: controls <=9'b000000100; //J
 			default: controls  <=9'bxxxxxxxxx; //???
 		endcase
 endmodule
 
 	
-
-module aludec (input [5:0] op, input [5:0] funct,
-					input [1:0] aluop,
-					output reg [2:0] alucontrol);
+module aludec (input [5:0] funct,
+				input [1:0] aluop,
+				output reg [2:0] alucontrol);
 					
 	always @ (*)
-		case (op) 
-		6'b001100:	alucontrol <= 3'b000; // ANDI
-		6'b001101:	alucontrol <= 3'b001; // ORI
-		6'b001110:	alucontrol <= 3'b011; // XORI
-		default: case (aluop)
+		  case (aluop)
 			2'b00: alucontrol <= 3'b010; // add
 			2'b01: alucontrol <= 3'b110; // sub
 			default: case(funct) // RTYPE
@@ -108,26 +99,21 @@ module aludec (input [5:0] op, input [5:0] funct,
 				6'b100101: alucontrol <= 3'b001; // OR
 				6'b101010: alucontrol <= 3'b111; // SLT
 				default: alucontrol <= 3'bxxx; // ???
-			endcase
 		endcase
 	endcase
 endmodule
 
 
-
-
-
-
 module datapath (input clk, reset,
-						input memtoreg, pcsrc,
-						input alusrc, regdst,
-						input regwrite, jump,
-						input [2:0] alucontrol,
-						output zero,
-						output [31:0] pc,
-						input [31:0] instr,
-						output [31:0] aluout, writedata,
-						input [31:0] readdata);
+				input memtoreg, pcsrc,
+				input alusrc, regdst,
+				input regwrite, jump,
+				input [2:0] alucontrol,
+				output zero,
+				output [31:0] pc,
+				input [31:0] instr,
+				output [31:0] aluout, writedata,
+				input [31:0] readdata);
 							
 	wire [4:0] writereg;
 	wire [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
@@ -155,14 +141,12 @@ module datapath (input clk, reset,
 endmodule
 
 
-
-
 module regfile (input clk,
-					input we3,
-					input [4:0] ra1, ra2, wa3,
-					input [31:0] wd3,
-					output [31:0] rd1, rd2);
-					reg [31:0] rf[31:0];
+			input we3,
+			input [4:0] ra1, ra2, wa3,
+			input [31:0] wd3,
+			output [31:0] rd1, rd2);
+			reg [31:0] rf[31:0];
 	// three ported register file
 	// read two ports combinationally
 	// write third port on rising edge of clock
@@ -175,11 +159,10 @@ module regfile (input clk,
 endmodule
 
 
-
-
 module adder (input [31:0] a, b,output [31:0] y);
 		assign y=a + b;
 endmodule
+
 
 module sl2 (input [31:0] a,
 	output [31:0] y);
@@ -200,21 +183,9 @@ module flopr # (parameter WIDTH = 8)(input clk, reset,input [WIDTH-1:0] d,output
 endmodule
 
 
-
 module mux2 # (parameter WIDTH = 8)(input [WIDTH-1:0] d0, d1,input s,output [WIDTH-1:0] y);
 	assign y = s ? d1 : d0;
 endmodule
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -227,9 +198,6 @@ input [2:0] i_alu_control;				// Control signal
 
 output wire o_zero_flag;				// Zero flag 
 assign o_zero_flag = ~|o_result;
-
-
-
 
 always @(*) begin
 	// Start initialization:
@@ -266,9 +234,6 @@ always @(*) begin
 			begin
 				o_result = {32{1'bx}};	// x-state, (nor 1, nor 0)
 			end
-	endcase
-end
-
+	      endcase
+        end
 endmodule
-
-
