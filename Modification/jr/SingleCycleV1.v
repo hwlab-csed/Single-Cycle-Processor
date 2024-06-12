dcbf7d5a6320280098f7032796fb7b8d6ffed15a
Vaishnavi-Pai19 <133690178+Vaishnavi-Pai19@users.noreply.github.com>
2024-06-12 11:21:58 +0300

    Update SingleCycleV1.v

--- Modification/jr/SingleCycleV1.v
+++ Modification/jr/SingleCycleV1.v
@@ -186,7 +186,7 @@ endmodule
 
 module sl2 (input [31:0] a, output [31:0] y);
 	// shift left by 2
-	assign y = {a[29:01], 2'b00};
+	assign y = {a[29:0], 2'b00};
 endmodule
 
 module signext (input [15:0] a, output [31:0] y);
